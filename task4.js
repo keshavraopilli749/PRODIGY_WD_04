295af029953a2f4de560a5294fcb3df4563c43c9
Pilli Keshav Rao <keshavraoplacement@gmail.com>
2024-11-21 19:21:01 +0530

    task4.js

---
+++ task4.js
@@ -0,0 +1,6 @@
+// Contact Form Submission
+document.getElementById("contact-form")?.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for reaching out! I'll get back to you soon.");
+  });
+  
